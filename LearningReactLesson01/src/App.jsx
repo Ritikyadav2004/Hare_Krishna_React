08ba313f1f7a29c560b202ab1e709a4c708c0736
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-07-10 17:59:05 +0530

    first commit

---
+++ LearningReactLesson01/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello React Iam very greatFull that I have finally Started Learning React Library and would Love i get a chance to learn from Shrikant sir #Gamma Bhiya</h1>
+    </>
+  )
+}
+
+export default App
