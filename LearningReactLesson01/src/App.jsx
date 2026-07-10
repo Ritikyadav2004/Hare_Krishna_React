62926454bea60e8d6f4e6003bbdc024b168d514c
Ritikyadav2004 <ritikyadav5000@gmail.com>
2026-07-10 18:51:54 +0530

    Lecture 3 learnings

--- LearningReactLesson01/src/App.jsx
+++ LearningReactLesson01/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import Chai from './Chai.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
+   <>
       <h1>Hello React Iam very greatFull that I have finally Started Learning React Library and would Love i get a chance to learn from Shrikant sir #Gamma Bhiya</h1>
+    <Chai/>
     </>
   )
 }
---
+++ LearningReactLesson01/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai()
+{
+    return(
+        <h1>Hey There iam from Chai Component</h1>
+    )
+}
+
+
+export default Chai;
